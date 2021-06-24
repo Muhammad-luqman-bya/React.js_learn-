302797f1e82d1f6cf897eb055af7f72887245591
Muhammad-luqman-bya <halloluqman0303@gmail.com>
2021-06-23 21:52:27 -0700

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react"; 
+import SecClass from './SecClass';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h3>We use Props within parent and child system  </h3>
+      <SecClass class="10th class Exam" time="21th" />
+      <br />
+      <hr/>
+      <SecClass class="11th class Exam"  time="22th" /> 
+      <br />
+      <hr/>
+      <SecClass class="12th class Exam"  time="23th" /> 
+      <br />
+      <hr/>
+    </div>     
   );
 }
 
---
+++ src/SecClass.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SecClass = (props) => {
+return(
+    <div>
+ <h3>After 1 month govt takes {props.class} and result will annowce on {props.time}</h3>
+</div>
+)
+}
+export default SecClass; 
